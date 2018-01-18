1e83fdfc9463b0b69f8989074c518dc6dccd14cd
271560sj <2607204977@qq.com>
2018-01-18 10:29:49 +0800

    增加修改keys,删除keys

--- src/workers/workers.go
+++ src/workers/workers.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 	"encoding/json"
 	"context"
+	"fmt"
 )
 
 type Worker struct {
@@ -43,25 +44,69 @@ func InitWorker(name,ip string,endpoints []string)  *Worker{
 
 func (w *Worker)WriterInfosBeat()  {
 	keysApi := w.KeysAPI
-	i := 0
+	i := 1
 	for  {
-		ids := "books" + strconv.Itoa(i)
-		keyWords := "book"
-		infos := "it is a book about num of " + strconv.Itoa(i)
-		i ++
-		wkInfos := &WorkInfo{
-			IDs: ids,
-			KeyWord: keyWords,
-			Infos: infos,
+		fmt.Printf("Send book infos of %d \n",i)
+		sendBookInfos(i,keysApi)
+		if i % 2 == 0 {
+			fmt.Printf("Change book infos of %d \n",i)
+			changeBookInfos(i,keysApi)
 		}
-		key := "books/" + ids
-		value,_ := json.Marshal(wkInfos)
-		_,err := keysApi.Set(context.Background(),key,string(value),&etcd.SetOptions{
-			TTL: time.Second * 10,
-		})
-		if err != nil {
-			 log.Println("Error send book infos",err)
+		if i % 3 ==0 {
+			fmt.Printf("Delete book infos of %d \n",i)
+			deleteBookInfos(i,keysApi)
+		}
+		i ++
+		if i > 30 {
+			break
 		}
 		time.Sleep(time.Second * 3)
+	}
+}
+
+func sendBookInfos(i int,keysApi etcd.KeysAPI)  {
+	ids := "books" + strconv.Itoa(i)
+	keyWords := "book"
+	infos := "it is a book about num of " + strconv.Itoa(i)
+	i ++
+	wkInfos := &WorkInfo{
+		IDs: ids,
+		KeyWord: keyWords,
+		Infos: infos,
+	}
+	key := "books/" + ids
+	value,_ := json.Marshal(wkInfos)
+	_,err := keysApi.Set(context.Background(),key,string(value),&etcd.SetOptions{
+		TTL: time.Second * 10,
+	})
+	if err != nil {
+		log.Println("Error send book infos",err)
+	}
+	log.Println("Send book infos success,",string(value))
+}
+func deleteBookInfos(i int,keysApi etcd.KeysAPI)  {
+	ids := "books" + strconv.Itoa(i)
+	keys := "books/" + ids
+	value,err := keysApi.Delete(context.Background(),keys,nil)
+	if err != nil {
+		log.Println("Error delete keys,",err)
+	}else {
+		log.Println("Deletes keys success",value)
+	}
+}
+func changeBookInfos(i int, keysApi etcd.KeysAPI )  {
+	ids := "books" + strconv.Itoa(i)
+	keys := "books/" + ids
+	wkInfos := &WorkInfo{
+		IDs: ids,
+		KeyWord: "book",
+		Infos: "update the book's infos",
+	}
+	values,_ := json.Marshal(wkInfos)
+	value,err := keysApi.Set(context.Background(),keys,string(values),nil)
+	if err != nil {
+		log.Println("Error update book's infos",err)
+	}else {
+		log.Println("Success update book's infos",value)
 	}
 }
